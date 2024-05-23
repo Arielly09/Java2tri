00c9e86da8dbea555210b559008813b551707cb0
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-05-23 08:33:38 -0300

    Add files via upload

---
+++ map-string.js
@@ -0,0 +1,5 @@
+const nomes = ["Kiko","Zinho","lindo"];
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
---
+++ set.js
@@ -0,0 +1,7 @@
+const nome = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nome);
+
+const nomesAtualizados = [...new Set(nome)];
+
+console.log(meuSet);
